f1b912e13e43588c0fe680f38dd16b8b56ed3575
Umair Khan <97866361+Bash-UK@users.noreply.github.com>
2023-06-01 11:26:13 +0530

    Update ds_lab11.scala

--- ds_lab11.scala
+++ ds_lab11.scala
@@ -1,34 +1,24 @@
 import scala.io.Source
 
-object WeatherProcessor {
-  def main(args: Array[String]): Unit = {
-    val filename = "weatherHistory.csv"
+val filename = "weatherHistory.csv"
+val lines = Source.fromFile(filename).getLines().toList
 
-    val lines = Source.fromFile(filename).getLines()
-
-
-    // Map
-    val data = lines.map(line => {
-      val fields = line.split(",")
-      val temperature = fields(3).toDouble
-      val dewPoint = fields(4).toDouble
-      val windSpeed = fields(6).toDouble
-      (temperature, dewPoint, windSpeed)
-    })
-
-
-    // Reduce
-    val (tempSum, dewPointSum, windSpeedSum, count) = data.foldLeft((0.0, 0.0, 0.0, 0)) {
-      case ((tempAcc, dewPointAcc, windSpeedAcc, countAcc), (temperature, dewPoint, windSpeed)) =>
-        (tempAcc + temperature, dewPointAcc + dewPoint, windSpeedAcc + windSpeed, countAcc + 1)
-    }
-
-    val avgTemp = tempSum / count
-    val avgDewPoint = dewPointSum / count
-    val avgWindSpeed = windSpeedSum / count
-
-    println(s"Average temperature: $avgTemp")
-    println(s"Average dew point: $avgDewPoint")
-    println(s"Average wind speed: $avgWindSpeed")
-  }
+val (tempSum, dewSum, windSum, count) = lines.foldLeft((0.0, 0.0, 0.0, 0)) {
+  case ((tempAcc, dewAcc, windAcc, countAcc), line) =>
+    val fields = line.split(",")
+    val temp = fields(3).toDouble
+    val dewPoint = fields(4).toDouble
+    val wind = fields(6).toDouble
+    (
+      tempAcc + temp,
+      dewAcc + dewPoint,
+      windAcc + wind,
+      countAcc + 1
+    )
 }
+
+println(s"Sum of temperature: $tempSum")
+println(s"Sum of dew point: $dewSum")
+println(s"Sum of wind speed: $windSum")
+println(s"Count of records: $count")
+
